a13272afac5771febac0b9a8719da27f80ee2cd7
Samuel Schwager <samuel.schwager@gmail.com>
2020-12-12 16:37:52 +0100

    day 10 (part 1) solved

---
+++ Day10/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Day10
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    using System.IO;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = File.ReadAllLines("input.txt").Select(Int32.Parse).ToList();
+            var deltas = GetDeltas(numbers);
+
+            var ones = deltas.Count(x => x == 1);
+            var threes = deltas.Count(x => x == 3) + 1; // outlet, adapters and device
+
+            Console.WriteLine($"Result Part 1 - {ones * threes}");
+        }
+
+        // Part 1
+        public static IEnumerable<int> GetDeltas(IEnumerable<int> numbers)
+        {
+            var ordered = numbers.OrderBy(x => x).ToList();
+            ordered.Insert(0, 0); // outlet is at 0 jolts
+
+            var deltas = ordered.Select((value, index) =>
+            {
+                var next = ordered.Skip(index + 1).Take(1).FirstOrDefault();
+                var delta = next - value;
+                return delta;
+            }).Where(x => x >= 0).ToList(); // the last delta is negative because of the default value
+
+            return deltas;
+        }
+    }
+}
